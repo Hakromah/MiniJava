2e1bd7fdcec266024e76db5dc6dfb5b286c60fe0
Hassan <kkromah12@gmail.com>
2023-06-26 18:22:29 +0300

    My first java commit

---
+++ src/javabasics/_2/Main.java
@@ -0,0 +1,24 @@
+package javabasics._2;
+
+import java.sql.SQLOutput;
+
+public class Main {
+
+   public static void main(String[] args) {
+        System.out.println("Hello Hassan");
+       exerciseTwo();
+       exerciseThree();
+
+   }
+
+   public static void exerciseTwo() {
+        System.out.print("Hello ");
+        System.out.print("Hassan ");
+   }
+
+   public static void exerciseThree() {
+        System.out.print("This should");
+        System.out.print("all appear");
+        System.out.print("on the same line");
+   }
+}
---
+++ src/javabasics/_3/Main.java
@@ -0,0 +1,12 @@
+package javabasics._3;
+
+public class Main {
+    public static void main(String[] args) {
+        //There are 8 primitive type variables in java:
+        //Essential primitives: int, boolean, char, double
+        //Useful primitives: float, short, byte, long
+
+
+        System.out.println(5);
+    }
+}
